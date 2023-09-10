4f2b4bf1ae219d16e8efa95bb5670547d1e339e9
Yarpen <71726390+Yarpen35@users.noreply.github.com>
2023-09-10 15:25:14 +0200

    new project structure

--- src/library/app/Library.java
+++ src/library/app/Library.java
@@ -1,23 +1,29 @@
 package library.app;
 
-import library.io.DataReader;
 import library.model.Book;
 
 public class Library {
-    public static void main(String[] args) {
-        final String appName = "Biblioteka v0.8";
 
-        Book[] books = new Book[1000];
-        DataReader dataReader = new DataReader();
+    private final int maxBooks = 1000;
+    private Book[] books = new Book[maxBooks];
+    private int booksNumber;
 
-        System.out.println(appName);
-        System.out.println("Wprowadź nową książkę:");
-        books[0] = dataReader.readAndCreateBook();
-        books[1] = dataReader.readAndCreateBook();
-        dataReader.close();
+    public void addBook(Book book) {
+        if (booksNumber < maxBooks) {
+            books[booksNumber] = book;
+            booksNumber++;
+        } else {
+            System.out.println("Maksymalna liczba książek została osiągnięta");
+        }
 
-        books[0].printInfo();
-        books[1].printInfo();
-        System.out.println("System może przechowywać do " + books.length + " książek");
+    }
+
+    public void printBooks() {
+        if (booksNumber == 0) {
+            System.out.println("Brak książek w bibliotece");
+        }
+        for (int i = 0; i < booksNumber; i++) {
+            books[i].printInfo();
+        }
     }
 }
